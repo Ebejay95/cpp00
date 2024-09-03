e97849638cc7fb2fcfe43818edefc6e1f9932c5d
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-09-03 22:58:15 +0000

    set up the megaphone with iterations over args  using toupper and the default std::cout output

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,38 @@
 /*   By: jeberle <jeberle@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 22:22:46 by jeberle           #+#    #+#             */
-/*   Updated: 2024/09/03 22:53:09 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/09/03 22:57:01 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+				j++;
+			}
+			if (argc - i > 1)
+				std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
