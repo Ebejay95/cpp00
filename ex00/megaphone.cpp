497fd60858887b9a702f500a4d1fc015ae44c6fa
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-09-03 22:54:12 +0000

    figured out how to compile c++ and build a hello world clean

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,38 +6,13 @@
 /*   By: jeberle <jeberle@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 22:22:46 by jeberle           #+#    #+#             */
-/*   Updated: 2024/09/03 22:50:53 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/09/03 22:53:09 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
 
-int main(int argc, char **argv)
-{
-	int i;
-	int j;
-
-	i = 1;
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
-	else
-	{
-		while (i < argc)
-		{
-			j = 0;
-			while (argv[i][j] != '\0')
-			{
-				std::cout << static_cast<char>(std::toupper(argv[i][j]));
-				j++;
-			}
-			if (argc - i > 1)
-				std::cout << " ";
-			i++;
-		}
-		std::cout << std::endl;
-	}
+int main() {
+	std::cout << "Hello, World!" << std::endl;
 	return (0);
 }
